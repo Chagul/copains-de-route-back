8a764c1673360d48b628764818aceb698c3e4537
Lucas Plé <lucas.ple.etu@univ-lille.fr>
2023-11-07 17:11:02 +0100

    Initial commit

---
+++ src/main/java/com/univ/lille/copainsderoute/platine/PlatineApplication.java
@@ -0,0 +1,13 @@
+package com.univ.lille.copainsderoute.platine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univ/lille/copainsderoute/platine/PlatineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univ.lille.copainsderoute.platine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
