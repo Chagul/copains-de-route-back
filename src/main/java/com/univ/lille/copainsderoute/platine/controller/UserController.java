7fc4e53ec2b677d384de769d4783c9819628275d
kesraoui <nassima.kesraoui@vadesecure.com>
2023-11-21 16:22:46 +0100

    oppps

---
+++ src/main/java/com/univ/lille/copainsderoute/platine/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.univ.lille.copainsderoute.platine.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    
+
+}
