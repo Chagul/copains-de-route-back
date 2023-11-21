230483ee05f51ea7e838d6c6042ea01d59fd1206
Lucas Plé <lucas.ple.etu@univ-lille.fr>
2023-11-21 10:14:13 +0100

    added Itinerary entity with a ManyToOne relation between Itinerary and Event

---
+++ src/main/java/com/univ/lille/copainsderoute/platine/entity/Itinerary.java
@@ -0,0 +1,24 @@
+package com.univ.lille.copainsderoute.platine.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Itinerary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int order;
+    private String point;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+}
