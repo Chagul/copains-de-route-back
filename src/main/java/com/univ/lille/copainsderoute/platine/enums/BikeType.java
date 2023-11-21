42504368ffecf7ff71ed11cd1a17f860df2e349e
Lucas Plé <lucas.ple.etu@univ-lille.fr>
2023-11-21 09:57:18 +0100

    added Event entity with RoadTYpe, BikeType and Visibility enums

---
+++ src/main/java/com/univ/lille/copainsderoute/platine/enums/BikeType.java
@@ -0,0 +1,9 @@
+package com.univ.lille.copainsderoute.platine.enums;
+
+public enum BikeType {
+    CITY,
+    MOUNTAIN,
+    ALL_TERRAIN,
+    GRAVEL,
+    BMX
+}
---
+++ src/main/java/com/univ/lille/copainsderoute/platine/enums/RoadType.java
@@ -0,0 +1,10 @@
+package com.univ.lille.copainsderoute.platine.enums;
+
+public enum RoadType {
+    DIRT,
+    GRAVEL,
+    PAVING_STONE,
+    ROAD,
+    CYCLE_PATH,
+    OTHERS
+}
