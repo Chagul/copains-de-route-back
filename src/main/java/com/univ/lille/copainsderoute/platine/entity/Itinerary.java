e57cb5a0bd020c089afef1c7345ff3bbeff44b0c
kesraoui <nassima.kesraoui@vadesecure.com>
2023-11-27 14:44:01 +0100

    Correction des entity

--- src/main/java/com/univ/lille/copainsderoute/platine/entity/Itinerary.java
+++ src/main/java/com/univ/lille/copainsderoute/platine/entity/Itinerary.java
@@ -1,7 +1,5 @@
 package com.univ.lille.copainsderoute.platine.entity;
 
-import com.univ.lille.copainsderoute.platine.dtos.ItineraryRequestDTOs;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
