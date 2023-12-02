1b18cf2db1960c96de6a59c13f58d740a8e4f95a
kesraoui <nassima.kesraoui@vadesecure.com>
2023-12-02 18:43:28 +0100

    changement du controller

--- src/main/java/com/univ/lille/copainsderoute/platine/controller/EventController.java
+++ src/main/java/com/univ/lille/copainsderoute/platine/controller/EventController.java
@@ -3,6 +3,7 @@ package com.univ.lille.copainsderoute.platine.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,24 +63,16 @@ public class EventController {
         return ResponseEntity.ok("Event deleted");
     }
     
-   @GetMapping("{northeastLatitude}/{northeastLongitude}/{southwestLatitude}/{southwestLongitude}/{northwestLatitude}/{northwestLongitude}/{southeastLatitude}/{southeastLongitude}")
-    public ResponseEntity<List<Event>> getEventsByLocation(
-            @PathVariable("northeastLatitude") Double northeastLatitude,
-            @PathVariable("northeastLongitude") Double northeastLongitude,
-            @PathVariable("southwestLatitude") Double southwestLatitude,
-            @PathVariable("southwestLongitude") Double southwestLongitude,
-            @PathVariable("northwestLatitude") Double northwestLatitude,
-            @PathVariable("northwestLongitude") Double northwestLongitude,
-            @PathVariable("southeastLatitude") Double southeastLatitude,
-            @PathVariable("southeastLongitude") Double southeastLongitude
-    ) throws RuntimeException {
 
-        GpsCoordinatesDTOs gpsCoordinatesDTOs = new GpsCoordinatesDTOs(northeastLatitude, northeastLongitude, southwestLatitude, southwestLongitude, northwestLatitude, northwestLongitude, southeastLatitude, southeastLongitude);
-        List<Event> itinerary = eventService.getEventsByLocation(gpsCoordinatesDTOs);
-        if (itinerary == null) {
+    @GetMapping("/location")
+    public ResponseEntity<List<Event>> getEventsByLocation(@RequestBody GpsCoordinatesDTOs gpsCoordinatesDTO) throws RuntimeException {
+        
+        List<Event> itinerary = eventService.getEventsByLocation(gpsCoordinatesDTO);
+        if (itinerary == null || itinerary.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(itinerary);
     }
+    
     }
 
