9eb7c3ed2558348debc4d705fb129a2af5635b63
Chagul <aurelien.plancke@hotmail.fr>
2023-12-30 16:15:50 +0100

    Filter events based on user doing the request

--- src/main/java/com/univ/lille/copainsderoute/platine/controller/EventController.java
+++ src/main/java/com/univ/lille/copainsderoute/platine/controller/EventController.java
@@ -37,12 +37,14 @@ public class EventController {
     private final JwtUtil jwtUtil;
 
     @GetMapping("")
-    public ResponseEntity<List<EventResponseDTOs>> getEvents() {
+    public ResponseEntity<List<EventResponseDTOs>> getEvents(HttpServletRequest request) {
         List<EventResponseDTOs> events = null;
         try {
-            events = eventService.getEvents();
+            events = eventService.getEvents(jwtUtil.getLogin(request));
         } catch (ZeroEventFoundException e) {
             return ResponseEntity.noContent().build();
+        } catch (UserNotFoundException e){
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(events);
     }
@@ -96,12 +98,15 @@ public class EventController {
     
 
     @PostMapping("location")
-    public ResponseEntity<List<EventResponseDTOs>> getEventsByLocation(@RequestBody GpsCoordinatesDTOs gpsCoordinatesDTO) {
+    public ResponseEntity<List<EventResponseDTOs>> getEventsByLocation(HttpServletRequest request, @RequestBody GpsCoordinatesDTOs gpsCoordinatesDTO) {
         List<EventResponseDTOs> itinerary = null;
         try {
-            itinerary = eventService.getEventsByLocation(gpsCoordinatesDTO);
+
+            itinerary = eventService.getEventsByLocation(gpsCoordinatesDTO, jwtUtil.getLogin(request));
         } catch (ZeroEventFoundException | EventNotfoundException e) {
             return ResponseEntity.noContent().build();
+        } catch (UserNotFoundException e) {
+           return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(itinerary);
     }
